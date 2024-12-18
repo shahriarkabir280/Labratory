1cbbe0f362e4c8c43795c8770bc806281c7674a4
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2024-12-18 14:32:11 +0600

    updated signup page

--- testapp/lib/authentications/signupScreen.dart
+++ testapp/lib/authentications/signupScreen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:testapp/authentications/validator.dart'; // Import the validators.dart file
 import 'package:testapp/authentications/termsofServicesandPrivacyPolicy.dart';
 import 'package:testapp/authentications/loginScreen.dart';
+import 'package:testapp/backend_connections/FASTAPI.dart';
+final FASTAPIhere FastAPIonthego = FASTAPIhere();
 
 class signupScreen extends StatefulWidget {
   @override
@@ -14,8 +16,38 @@ class _SignupScreenState extends State<signupScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
-
+  // fastapi ke msg pathacchi
+  // Future<void> sendDataTofastAPI(String email, String pass) async {
+  //   final String url = 'http://10.0.2.2:8000/save-input/'; // default android emu url diye connect hocchi
+  //
+  //   final response = await http.post(
+  //     Uri.parse(url),
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: json.encode({"email": email, "password":pass}),
+  //   );
+  //
+  //   if (response.statusCode == 200) {
+  //     final responseData = json.decode(response.body);
+  //
+  //     // Show message from the response
+  //     if(responseData['message']=="Successfully Registered")
+  //     {ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text(responseData['message']),backgroundColor: Colors.lightGreen),
+  //     );}
+  //     else {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(content: Text(responseData['message']),backgroundColor: Colors.redAccent),
+  //       );
+  //     }
+  //   } else {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text('Failed to save data')),
+  //     );
+  //   }
+  // }
   bool _isChecked = false;
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -69,14 +101,14 @@ class _SignupScreenState extends State<signupScreen> {
                 icon: Icons.email,
               ),
               const SizedBox(height: 15),
-              _buildTextField(
+              _buildTextField1(
                 controller: _passwordController,
                 label: "Password",
                 icon: Icons.lock,
                 isPassword: true,
               ),
               const SizedBox(height: 15),
-              _buildTextField(
+              _buildTextField2(
                 controller: _confirmPasswordController,
                 label: "Confirm Password",
                 icon: Icons.lock,
@@ -146,6 +178,7 @@ class _SignupScreenState extends State<signupScreen> {
                     _passwordController.text,
                     _confirmPasswordController.text,
                   );
+                  final inputText = _nameController.text;
 
                   if(nothavingName!=null){
                     Validators.showSnackBar(context, nothavingName);
@@ -172,8 +205,17 @@ class _SignupScreenState extends State<signupScreen> {
                     return;
                   }
 
-                  // If all validations pass
-                  Validators.showSnackBar(context, "Sign-Up Successful!", backgroundColor: Colors.green);
+                  if (_emailController.text.isNotEmpty) {
+                    FastAPIonthego.user_register(context,_nameController.text, _emailController.text , _passwordController.text);// Send the input text to the FastAPI backend
+                    } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Please enter Username')),
+                    );
+                  }
+
+                  //all ok
+                  // Validators.showSnackBar(context, "Successfully registered", backgroundColor: Colors.lightGreen);
+
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.teal[700],
@@ -242,6 +284,89 @@ class _SignupScreenState extends State<signupScreen> {
           borderSide: BorderSide(color: Colors.teal[300]!),
           borderRadius: BorderRadius.circular(10),
         ),
+
+      ),
+    );
+  }
+
+  // helper widget for password text field
+  Widget _buildTextField1({
+    required TextEditingController controller,
+    required String label,
+    required IconData icon,
+    bool isPassword = false,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: isPassword ? !_isPasswordVisible : false,
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.teal),
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.teal[600]),
+        filled: true,
+        fillColor: Colors.white,
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.teal[700]!),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.teal[300]!),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        suffixIcon: isPassword
+            ? IconButton(
+          icon: Icon(
+            _isPasswordVisible ? Icons.visibility_off : Icons.visibility,
+            color: Colors.teal,
+          ),
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
+        )
+            : null,
+      ),
+    );
+  }
+
+  //helper text field for confirm password field
+  Widget _buildTextField2({
+    required TextEditingController controller,
+    required String label,
+    required IconData icon,
+    bool isPassword = false,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: isPassword ? !_isConfirmPasswordVisible : false,
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.teal),
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.teal[600]),
+        filled: true,
+        fillColor: Colors.white,
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.teal[700]!),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.teal[300]!),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        suffixIcon: isPassword
+            ? IconButton(
+          icon: Icon(
+            _isConfirmPasswordVisible ? Icons.visibility_off : Icons.visibility,
+            color: Colors.teal,
+          ),
+          onPressed: () {
+            setState(() {
+              _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+            });
+          },
+        )
+            : null,
       ),
     );
   }
